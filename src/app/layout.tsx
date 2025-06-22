083f9c04236b03305584e8e2131d14f053d89d44
Eynar J. Alvarez M. <115285455+Eynar92@users.noreply.github.com>
2025-06-22 19:05:31 -0400

    Update Metadata and FIrst Column Name (#6)
    * udpate: application description
    * update: metadata and first column name from 'verb' to 'base form'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,27 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "BabeLift",
-  description: "A dynamic reference tool for English verb forms: base, third person singular, gerund (-ing), simple past, past participle, and gerund.",
+  title: "BabeLift | English Verb Conjugation Tool",
+  description: "Master English verb conjugations with BabeLift! Practice and learn base form, 3rd person, gerund, simple past, past participle, and more in an interactive table. Perfect for ESL students and language learners.",
+  keywords: [
+    "English verbs",
+    "verb conjugation",
+    "ESL",
+    "learn English",
+    "past participle",
+    "gerund",
+    "irregular verbs",
+    "language learning"
+  ],
+  authors: [{ name: "Eynar Joel Alvarez M.", url: "https://irregular-regular-verbs.vercel.app" }],
+  creator: "Joel Alvarez",
+  publisher: "Joel Alvarez",
+  openGraph: {
+    title: "BabeLift | English Verb Conjugation Tool",
+    description: "Interactive tool to master English verb conjugations for all tenses.",
+    url: "https://irregular-regular-verbs.vercel.app",
+    siteName: "BabeLift"
+  },
 };
 
 export default function RootLayout({
